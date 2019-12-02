41b4a0e0830f54afafa26855a507608cd43a3de0
JoshuaHGeorge <jwatime@gmail.com>
2019-12-01 22:04:41 -0500

    Added Homework API

---
+++ COMP2084GAssignment2/Controllers/api/HomeworkController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2084GAssignment2.Models;
+
+namespace COMP2084GAssignment2.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeworkController : ControllerBase
+    {
+        private readonly PlannerContext _context;
+
+        public HomeworkController(PlannerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Homework
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Homework>>> GetHomework()
+        {
+            return await _context.Homework.ToListAsync();
+        }
+
+        // GET: api/Homework/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Homework>> GetHomework(int id)
+        {
+            var homework = await _context.Homework.FindAsync(id);
+
+            if (homework == null)
+            {
+                return NotFound();
+            }
+
+            return homework;
+        }
+
+        // PUT: api/Homework/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHomework(int id, Homework homework)
+        {
+            if (id != homework.HomeworkId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(homework).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HomeworkExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Homework
+        [HttpPost]
+        public async Task<ActionResult<Homework>> PostHomework(Homework homework)
+        {
+            _context.Homework.Add(homework);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHomework", new { id = homework.HomeworkId }, homework);
+        }
+
+        // DELETE: api/Homework/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Homework>> DeleteHomework(int id)
+        {
+            var homework = await _context.Homework.FindAsync(id);
+            if (homework == null)
+            {
+                return NotFound();
+            }
+
+            _context.Homework.Remove(homework);
+            await _context.SaveChangesAsync();
+
+            return homework;
+        }
+
+        private bool HomeworkExists(int id)
+        {
+            return _context.Homework.Any(e => e.HomeworkId == id);
+        }
+    }
+}
